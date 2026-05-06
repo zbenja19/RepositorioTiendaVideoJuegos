572e2339b09fad9599ce2089f128998376d21fd7
Karol <karo.carrasco@duocuc.cl>
2026-05-05 20:05:05 -0400

    Subiendo mi proyecto de videojuegos

---
+++ servicio-videojuego/src/main/java/com/tienda/servicio_videojuego/ServicioVideojuegoApplication.java
@@ -0,0 +1,13 @@
+package com.tienda.servicio_videojuego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioVideojuegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioVideojuegoApplication.class, args);
+	}
+
+}
