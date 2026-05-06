64cc2cf20de09702ed948fe1ce080a85668ef995
Karol <karo.carrasco@duocuc.cl>
2026-05-06 19:35:22 -0400

    Corregi el  nombre del archivo VideoJuegoRepository para coincidir con la interfaz

--- servicio-videojuego/src/main/java/com/tienda/servicio_videojuego/repository/VideojuegoRepository.java
+++ servicio-videojuego/src/main/java/com/tienda/servicio_videojuego/repository/VideojuegoRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VideojuegoRepository extends JpaRepository<VideoJuego,Long> {
+public interface VideoJuegoRepository extends JpaRepository<VideoJuego,Integer> {
 
 }
