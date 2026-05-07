a70adbea88f7293e8e8713d08293ae82fc0e1800
m4ti444 <matiasfloresp24@gmail.com>
2026-05-07 16:13:38 -0400

    service de categoria actualizado

--- servicio-videojuego/src/main/java/com/tienda/servicio_videojuego/service/CategoriaService.java
+++ servicio-videojuego/src/main/java/com/tienda/servicio_videojuego/service/CategoriaService.java
@@ -1,5 +1,45 @@
 package com.tienda.servicio_videojuego.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.tienda.servicio_videojuego.model.Categoria;
+import com.tienda.servicio_videojuego.repository.CategoriaRepository;
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
 public class CategoriaService {
+    
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public List <Categoria> listas (){
+        return categoriaRepository.findAll();
+    }
+
+    public Categoria guardar (Categoria categoria){
+        return categoriaRepository.save(categoria);
+            
+    }
+
+    public Categoria buscarCategoria(Integer, id){
+        try {
+            for (iterable_type iterable_element : iterable) {
+                
+            }
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+    
+
+
+
+
+
+
 
 }
