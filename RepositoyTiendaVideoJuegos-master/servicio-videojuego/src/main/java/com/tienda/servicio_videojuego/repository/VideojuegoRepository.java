fc6ea64dbfad1f34301de12b0da387d68a975f54
yusepe2000 <mat.oviedo@duocuc.cl>
2026-05-06 20:52:14 -0400

    v2 modelo

---
+++ RepositoyTiendaVideoJuegos-master/servicio-videojuego/src/main/java/com/tienda/servicio_videojuego/repository/VideojuegoRepository.java
@@ -0,0 +1,10 @@
+package com.tienda.servicio_videojuego.repository;
+
+import com.tienda.servicio_videojuego.model.VideoJuego;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class VideojuegoRepository {
+
+}
