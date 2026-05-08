c8dee17a45687adc37894d21d01c6d42027ddccd
Karol <karo.carrasco@duocuc.cl>
2026-05-08 19:24:57 -0400

    elimine carpeta

--- src/main/java/com/tienda/servicio_videojuego/repository/ProveedorRepository.java
+++ src/main/java/com/tienda/servicio_videojuego/repository/ProveedorRepository.java
@@ -2,7 +2,6 @@ package com.tienda.servicio_videojuego.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.tienda.servicio_videojuego.model.Proveedor;
 
 @Repository
