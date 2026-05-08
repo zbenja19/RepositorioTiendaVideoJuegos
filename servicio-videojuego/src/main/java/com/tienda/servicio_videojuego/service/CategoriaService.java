426e98459db0f5961642c0ebbea1f25ead3019c3
Karol <karo.carrasco@duocuc.cl>
2026-05-07 21:43:42 -0400

    guardar

--- servicio-videojuego/src/main/java/com/tienda/servicio_videojuego/service/CategoriaService.java
+++ servicio-videojuego/src/main/java/com/tienda/servicio_videojuego/service/CategoriaService.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.tienda.servicio_videojuego.model.Categoria;
 import com.tienda.servicio_videojuego.repository.CategoriaRepository;
+
 import jakarta.transaction.Transactional;
 
 @Service
@@ -20,26 +22,7 @@ public class CategoriaService {
     }
 
     public Categoria guardar (Categoria categoria){
-        return categoriaRepository.save(categoria);
-            
+        return categoriaRepository.save(categoria);       
     }
-
-    public Categoria buscarCategoria(Integer, id){
-        try {
-            for (iterable_type iterable_element : iterable) {
-                
-            }
-            }
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-    }
-    
-
-
-
-
-
-
 
 }
