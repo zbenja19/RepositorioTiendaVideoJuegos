d375362c490af434817e0318744e44b0294d31b9
yusepe2000 <mat.oviedo@duocuc.cl>
2026-05-08 01:16:40 -0400

    v3 tienda

---
+++ src/main/java/com/tienda/servicio_videojuego/repository/CarritoRepository.java
@@ -0,0 +1,13 @@
+package com.tienda.servicio_videojuego.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tienda.servicio_videojuego.model.Carro;
+
+@Repository
+public interface CarritoRepository extends JpaRepository<Carro,Integer> {
+
+    
+
+}
