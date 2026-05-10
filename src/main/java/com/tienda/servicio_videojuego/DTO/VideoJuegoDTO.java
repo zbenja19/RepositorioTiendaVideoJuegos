7f4e2d61575acaf3acd1c043ddada2b00d2c76fa
Karol <karo.carrasco@duocuc.cl>
2026-05-09 20:01:09 -0400

    Mis cambios de Service y DTO

--- src/main/java/com/tienda/servicio_videojuego/DTO/VideoJuegoDTO.java
+++ src/main/java/com/tienda/servicio_videojuego/DTO/VideoJuegoDTO.java
@@ -1,5 +1,20 @@
 package com.tienda.servicio_videojuego.DTO;
 
+import com.tienda.servicio_videojuego.model.Categoria;
+import com.tienda.servicio_videojuego.model.Plataforma;
+import com.tienda.servicio_videojuego.model.Proveedor;
+import lombok.Data;
+
+@Data
 public class VideoJuegoDTO {
+
+    private Integer id;
+    private String nombre;
+    private String descripcion;
+    private Double precio;
+    private Integer stock;
+    private Categoria categoria;
+    private Plataforma plataforma;
+    private Proveedor proveedor;
 
 }
